f9522278562ba9d6b41ab6d3f8d7bbf96415e260
Shameek Hargrave <shargraveseeds@gmail.com>
2023-03-23 21:51:48 -0400

    a2 upload

--- Assignments/Assignment2/PriceFeed.sol
+++ Assignments/Assignment2/PriceFeed.sol
@@ -5,5 +5,21 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./interfaces/IPriceFeed.sol";
 
 contract PriceFeed is IPriceFeed {
-    /* TODO: implement your functions here */
+    AggregatorV3Interface internal priceFeed;
+    // BNB / USD: 0x7b219F57a8e9C7303204Af681e9fA69d17ef626f
+    constructor() {
+        priceFeed = AggregatorV3Interface(
+            0x7b219F57a8e9C7303204Af681e9fA69d17ef626f
+        );
+    }
+    function getLatestPrice() external view returns (int, uint) {
+        (
+            uint80 roundID ,
+            int price,
+            uint startedAt,
+            uint updatedAt,
+            uint80 answeredInRound
+        ) = priceFeed.latestRoundData();
+        return (price, updatedAt);
+    }
 }
